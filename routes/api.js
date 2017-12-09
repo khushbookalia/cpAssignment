602f9a6539e1611397040902d4ed5b047301b713
khushboo21417 <khushboo.21417@lpu.co.in>
2017-12-09 13:30:09 +0530

    aaa

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const router=express.Router();//for subpaths
 //const authenticate=require('./allroutes/authenticate');
 const eventRoute = require('./allRoutes/events');
 
-router.use('/authenticate', authenticate);
-//router.use('/events', eventRoute);
+//router.use('/authenticate', authenticate);
+router.use('/events', eventRoute);
 
 module.exports=router;
