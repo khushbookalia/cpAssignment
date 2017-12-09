e4d195c5a6c79bdcc4c61e277d28c9c39e0e0483
khushboo21417 <khushboo.21417@lpu.co.in>
2017-12-09 22:22:43 +0530

    tt

--- routes/api.js
+++ routes/api.js
@@ -8,5 +8,10 @@ const eventUsers = require('./allRoutes/users')
 router.use('/eventUser',eventUsers);
 router.use('/authenticate', authenticate);
 router.use('/events', eventRoute);
-
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 module.exports=router;
